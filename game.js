c0b4b613e2021cdd9b5e73213d1564e1507976bb
sk-rw <123981682+sk-rw@users.noreply.github.com>
2023-03-24 15:00:59 -0500

    Add funtion to play 5 round game and declare the winner at he end.

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
+// Function to get the computer's choice
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
   
+  // Function to play a single round of Rock Paper Scissors
   function playRound(playerSelection, computerSelection) {
     // Make playerSelection case-insensitive
     playerSelection = playerSelection.toLowerCase();
@@ -33,6 +35,34 @@ function getComputerChoice() {
       return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
   }
+  
+  // Function to play a 5 round game of Rock Paper Scissors
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = prompt("Choose Rock, Paper, or Scissors:");
+      const computerSelection = getComputerChoice();
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes("win")) {
+        playerScore++;
+      } else if (result.includes("lose")) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log(`You win the game! ${playerScore} to ${computerScore}`);
+    } else if (computerScore > playerScore) {
+      console.log(`You lose the game! ${computerScore} to ${playerScore}`);
+    } else {
+      console.log("It's a tie game!");
+    }
+  }
+  
 
 
   
