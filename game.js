f8924a7f8de7f2a228495ea060ec1ae823e36053
sk-rw <123981682+sk-rw@users.noreply.github.com>
2023-03-24 14:43:40 -0500

    Add function to play a single round of Rock Paper Scissors

--- game.js
+++ game.js
@@ -4,3 +4,36 @@ function getComputerChoice() {
     return choices[randomIndex];
   }
   
+  function playRound(playerSelection, computerSelection) {
+    // Make playerSelection case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+  
+    // Define possible outcomes for the game
+    const outcomes = {
+      rock: {
+        winsAgainst: 'scissors',
+        losesAgainst: 'paper'
+      },
+      paper: {
+        winsAgainst: 'rock',
+        losesAgainst: 'scissors'
+      },
+      scissors: {
+        winsAgainst: 'paper',
+        losesAgainst: 'rock'
+      }
+    };
+  
+    // Determine the outcome of the game
+    if (playerSelection === computerSelection.toLowerCase()) {
+      return "It's a tie!";
+    } else if (outcomes[playerSelection].winsAgainst === computerSelection.toLowerCase()) {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+
+
+  
+  
